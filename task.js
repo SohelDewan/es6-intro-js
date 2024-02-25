c8b9aa6ee759ee5bb1b8052277b34f63e2d8732c
SohelDewan <sohelds241@gmail.com>
2024-02-25 12:10:24 +0600

    All task has been done

--- task.js
+++ task.js
@@ -10,9 +10,6 @@ const friendFind = (num)=>{
     }
     return newFriends;
 }
-// friendFind(friends);
-
-// console.log(newFriends);
 
 const elements = [2, 3, 4, 5, 6, 7, 8, 9];
 const squareArr = [];
@@ -32,3 +29,26 @@ const square = (elements)=>{
     return avg;
 }
 square(elements);
+// arrow functions will take two arrays inputs and return max
+const newArr = [12, 34, 43, 21, 9, 13];
+// const twoArr = [...elements, ...newArr];
+// console.log(twoArr);
+
+const combinedArrMax = (arr1, arr2) => {
+    const newArr = [...arr1, ...arr2];
+    const max = Math.max(...newArr);
+    return max;
+};
+// console.log(combinedArrMax(elements, newArr));
+// How to make even numbers by mapping?
+const oddArray = [1, 3, 5, 7, 9,];
+const evenArray = oddArray.map(number => number+1);
+// console.log(evenArray);
+const givenArray = [30, 50, 79, 79, 90, 101, 30];
+
+
+const divisibleByTen = array =>{
+    return array.filter(array => array%10 === 0);
+}
+const expectedArray = divisibleByTen(givenArray); 
+console.log(expectedArray);
